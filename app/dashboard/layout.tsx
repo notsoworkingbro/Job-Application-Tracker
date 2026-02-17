56168f2974020d6a7700deea73fe2203ce8064bb
notsoworkingbro <workingamap@gmail.com>
2026-02-17 22:57:58 +0800

    added NavBar

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,22 @@
+
+import Navbar from "@/components/Navbar"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+
+        <Navbar />
+
+        <main className="max-w-7xl mx-auto p-4">
+          {children}
+        </main>
+
+      </body>
+    </html>
+  )
+}
