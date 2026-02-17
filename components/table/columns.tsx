3fbb278deef4eb799b69bd0df9aacafa44424eca
notsoworkingbro <workingamap@gmail.com>
2026-02-17 23:08:34 +0800

    added type Application data and columns

---
+++ components/table/columns.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+export type Application = {
+  company: string
+  position: string
+  status: "Offer" | "Applied" | "Interviewed" | "Rejected" | "Pending"
+  application_date: string
+  salary: number
+}
+
+export const columns: ColumnDef<Application>[] = [
+  {
+    accessorKey: "company",
+    header: "Company",
+  },
+  {
+    accessorKey: "position",
+    header: "Position",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "application_date",
+    header: "Application Date",
+  },
+  {
+    accessorKey: "salary",
+    header: "Salary",
+  },
+]
