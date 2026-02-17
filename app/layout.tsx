1babfc1673b90906397faf4bf6f0505a60d1eb3f
notsoworkingbro <workingamap@gmail.com>
2026-02-17 22:51:52 +0800

    removed google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,5 @@
 import { ClerkProvider } from '@clerk/nextjs'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export default function RootLayout({
   children,
@@ -20,7 +9,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <body>
           {children}
         </body>
       </html>
