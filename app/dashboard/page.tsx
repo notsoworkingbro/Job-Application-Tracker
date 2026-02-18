aa603330e779fb27fe7fc3dd30916362f49ce74e
notsoworkingbro <workingamap@gmail.com>
2026-02-18 23:36:57 +0800

    connected neon postgresql to DataTable component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,26 +1,14 @@
-"use client"
-
-import { columns, Application } from "@/components/table/columns"
+import { columns} from "@/components/table/columns"
 import { DataTable } from "@/components/table/data-table"
 
-const data: Application[] = [
-  {
-    company: "Google",
-    position: "Frontend Developer",
-    status: "Applied",
-    application_date: "2026-02-10",
-    salary: 80000,
-  },
-  {
-    company: "Microsoft",
-    position: "Backend Engineer",
-    status: "Interviewed",
-    application_date: "2026-02-01",
-    salary: 90000,
-  },
-]
+import { applications } from "@/lib/db/schema"
+import { db } from "@/lib/db/db"
 
-export default function Page() {
+export default async function Page() {
+
+  // 🔥 Fetch data from Neon database
+  const data = await db.select().from(applications)
+
   return (
     <div className="p-6">
       <DataTable columns={columns} data={data} />
