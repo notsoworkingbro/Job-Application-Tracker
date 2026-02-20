819ac385e6df7072ee4b842206b8bba603f71759
notsoworkingbro <workingamap@gmail.com>
2026-02-20 23:29:35 +0800

    removed interface Prop and used InferSelectModel exported from lib/db/schema.ts

--- lib/job-applications.ts
+++ lib/job-applications.ts
@@ -1,34 +1,26 @@
-"use client"
- 
 import { ColumnDef } from "@tanstack/react-table"
- 
-export type Application = {
-    company: string,
-    position: string,
-    status: "Offer" | "Applied" | "Interviewed" | "Rejected" | "Pending"
-    application_date: string,
-    salary: number,
-}
+import type { Application } from "@/lib/db/schema"
 
 export const columns: ColumnDef<Application>[] = [
-    {
-        accessorKey: "company",
-        header: "Company",
-    },
-    {
-        accessorKey: "position",
-        header: "Position",
-    },
-    {
-        accessorKey: "status",
-        header: "Status",
-    },
-    {
-        accessorKey: "application_date",
-        header: "Application Date",
-    },
-    {
-        accessorKey: "salary",
-        header: "Salary",
-    },
+   
+  {
+    accessorKey: "company",
+    header: "Company",
+  },
+  {
+    accessorKey: "position",
+    header: "Position",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "application_date",
+    header: "Application Date",
+  },
+  {
+    accessorKey: "salary",
+    header: "Salary",
+  },
 ]
