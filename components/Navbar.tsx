95855f6bc86e8efb24d9747a225d09f0c5b92d83
notsoworkingbro <workingamap@gmail.com>
2026-02-24 23:01:58 +0800

    .

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,14 +6,14 @@ import { SignedIn, UserButton } from '@clerk/nextjs'
 export default function Navbar() {
   return (
     <nav className="w-full border-b bg-white shadow-sm">
-      <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
+      <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-center gap-6">
 
         <Link href="/" className="text-xl font-bold">
           I Need A Job
         </Link>
         
         <SignedIn>
-          <UserButton />
+          <UserButton afterSignOutUrl="/dashboard"/>
         </SignedIn>
 
 
