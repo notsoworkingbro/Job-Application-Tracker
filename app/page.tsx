da3b8538820f70c0576d693613ed0af2566e595e
notsoworkingbro <workingamap@gmail.com>
2026-02-24 22:54:14 +0800

    .

--- app/page.tsx
+++ app/page.tsx
@@ -12,15 +12,6 @@ export default function Home() {
       <SignedIn>
         {redirect("/dashboard")}
       </SignedIn>
-
-      {/* if signed out show sign-in */}
-      <SignedOut>
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
-          <main className="w-full max-w-md rounded-lg bg-white p-8 shadow dark:bg-black">
-            <SignIn forceRedirectUrl="/dashboard" />
-          </main>
-        </div>
-      </SignedOut>
     </>
   );
 }
